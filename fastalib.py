0d7c877555b9d17c64bde168a39b02ff7c89e4b7
Jason Charamis <125705059+JasonCharamis@users.noreply.github.com>
2023-09-01 19:56:37 +0300

    Update fastalib.py

--- fastalib.py
+++ fastalib.py
@@ -1,96 +1,167 @@
-from Bio import SeqIO
-from Bio.Seq import Seq
-import re
+from natsort import natsorted
+import argparse
+import re, os
 
-## writes a fasta dictionary to file ##
-def write_file (dict,filename):   
-    with open(filename, "w") as outfile:
-        for id in dict.keys():
-            outfile.write(">"+id+"\n"+dict[id]+"\n")
 
-## converts fasta to 1l and writes into file ##            
-def fasta_1l(fasta):
-    genes={}
-    for seq_record in SeqIO.parse(fasta, "fasta"):
-        name=re.sub(".*/| .*","",str(seq_record.id))
-        name=re.sub(".p","_p/",name)
-        sequence=re.sub("\*|\--","",str(seq_record.seq))
-        genes[name]=sequence
-        write_file(genes,str(fasta+".1l"))
-    return genes
+def check_isfile ( input_file ): ## Function to check if input is a file or a string
+    if os.path.isfile(input_file):
+        glist = []
+        
+        with open(input_file, "r") as file:
+            lines = file.readlines()
+            for line in lines:
+                glist.append(line.strip())
+        return glist
+        
+    elif isinstance(input_file, str):
+        glists = input_file
+        return glists
+
+
+class FASTA:
+
+    __slots__ = ['id','seq']
+    
+    def __init__ (self, header, sequence):
+        self.id = header
+        self.seq = sequence
+
+
+    def __str__ ( self ) :
+        return f">{self.id}\n{self.seq}"
     
 
-## custom fasta parser with base python ##
-def fasta_parser ( fasta_file ):
-    seqs = {}
-    header = ""
-    sequence = ""   
+    def fasta_parser ( fasta_file ): ## opens fasta file and creates new FASTA instances 
+        fasta_instances = []
 
-    with open ( fasta_file, "r" ) as file:
-        for fasta in file:
-            fasta=fasta.strip ("\n")
-        
-            if fasta.startswith(">"):
-                header=re.sub(">","",fasta)
-                seqs[header]=sequence
-      
-            elif not re.search ("\--",fasta):
-                sequence += fasta
-                seqs[header] = sequence
+        with open ( fasta_file , 'r' ) as fasta:
+            lines = fasta.readlines()
+
+            for line in lines:
+                line = line.strip('\n')
                 sequence = ""
-    return seqs
 
+                if line.startswith('>'):
+                    id = re.sub ( ">", "", line)
+                        
+                elif not re.search ("\--|#",line):
+                    sequence += line
 
-## sizer of fasta sequences, takes dictionary as input ##
-def fasta_sizer ( fasta_file, mode = True):
-    sizer = {}
+                if id and sequence:
+                    fasta_instance = FASTA(id, sequence)                   
+                    fasta_instances.append(fasta_instance)
+                    
+            return ( fasta_instances )
 
-    for id,seq in fasta_1l(fasta_file):
-        sizer[id] = len( seq )
-        sort = dict ( sorted ( sizer.items(), key = lambda x:x[1], reverse = mode ) )
-
-    for pr,size in sort.items():
-        print ( pr+"\t",size )
         
-    return
+    def fasta_sizer ( fasta_file): ## sizer of fasta sequences, takes dictionary as input ##
+        lst = []
+        fasta_instances = FASTA.fasta_parser(fasta_file)
+
+        # Define a custom sorting key function
+        for fasta_instance in fasta_instances:
+            fasta_size = '\t'.join([fasta_instance.id, str(len(fasta_instance.seq))])
+            lst.append (fasta_size)
+           
+        sorted_list = sorted(lst, key=lambda x: x[1], reverse=True) # sort based on sequence length
+       
+        return sorted_list
+
+       
+    def fasta_extract ( fasta_file, gene_list ):
+        subset = []
+    
+        fasta_instances = FASTA.fasta_parser(fasta_file)
+
+        # Define a custom sorting key function
+        for fasta_instance in fasta_instances:
+            
+            if check_isfile ( gene_list ):
+                if fasta_instance.id in check_isfile ( gene_list ) :
+                    subset.append ( fasta_instance )
+            else:
+                if re.search ( gene_list, fasta_instance.id ):
+                    subset.append ( fasta_instance )
+              
+        if len(subset) > 0:            
+            return subset
+        
+        else:
+            print ( "Gene list is empty or not provided." )
+
+            
+    def fasta_remove ( fasta_file, gene_list ):
+        subset = []
+        fasta_instances = FASTA.fasta_parser(fasta_file)
+
+        # Define a custom sorting key function
+        for fasta_instance in fasta_instances:            
+            if check_isfile ( gene_list ):
+                if fasta_instance.id not in check_isfile ( gene_list ):
+                    subset.append ( fasta_instance )
+            else:
+                if fasta_instance.id != gene_list:
+                    subset.append ( fasta_instance )
+              
+        if len(subset) > 0:            
+            return subset
+        
+        else:
+            print ( "Gene list is empty or not provided." )
+
+            
+
+## Implementation ##
+
+def kargs():
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser(description='Library for efficiently manipulating fasta files.')
+    parser.add_argument('-f','--fasta', type=str, help='FASTA file')
+    parser.add_argument('-o','--one_line', action="store_true", help='Option to extract FASTA sequences.')
+    parser.add_argument('-e','--extract', type=str, help='Option to extract FASTA sequences.')
+    parser.add_argument('-r','--remove', type=str, help='Option to remove FASTA sequences.')
+    parser.add_argument('-s','--size', action="store_true", help='Option to print sizes of FASTA sequences.')
+    args = parser.parse_args()
+
+    if not any(vars(args).values()):
+        parser.print_help()
+        print("Error: No arguments provided.")
+
+    return parser.parse_args()
 
 
-def fasta_filter ( fasta_file, filter_list ):
-    list = {}
+def main():
+    
+    parser = argparse.ArgumentParser(description='Library for efficiently manipulating fasta files.')
+    args = kargs()
+    
+    if args.fasta:
 
-    with open ( filter_list, "r" ) as file:
-        for l in file:
-            l = re.sub ( "\n", "", l )
-            if l not in list.keys():
-                list[l]=1
+        inp = re.sub (".fa$|.faa$|.fna$|.fasta$","",args.fasta)
+        
+        if args.one_line:
+            with open(f"{inp}.fasta.1l", "w") as f:
+                for out in FASTA.fasta_parser(args.fasta):
+                    print ( out, file = f )
 
-    for id,seq in fasta_parser(fasta_file).items():
-        if id not in list.keys():
-            print (">"+id + "\n" + fasta_parser(fasta_file)[id] )
+        elif args.size:
+            with open(f"{inp}.fasta.sizes", "w") as f:
+                for out in FASTA.fasta_sizer(args.fasta):
+                    print ( out )
+                    #print ( out, file = f )
 
-    return
+        elif args.extract:
+            with open(f"{inp}.{args.extract}.extracted.fasta", "w") as f:
+                for out in FASTA.fasta_extract(args.fasta, args.extract):
+                    print ( out )
 
+        elif args.remove:
+            with open(f"{inp}.{args.remove}.removed.fasta", "w") as f:
+                for out in FASTA.fasta_remove(args.fasta, args.remove):
+                    print ( out )
+    else:
+        print("Please provide either --one_line, --extract, --remove, or --size option.")
+               
 
-def extract_fasta ( fasta_file, extract_list ):
-    list = {}
-
-    with open ( extract_list, "r" ) as file:
-        for l in file:
-            l = re.sub ( "\n", "", l )
-            if l not in list.keys():
-                list[l]=1
-
-    for id,seq in fasta_parser(fasta_file).items():
-        if id in list.keys():
-            print (">"+id + "\n" + fasta_parser(fasta_file)[id] )
-
-    return
-
-
-def extract_fasta_headers ( fasta_file ):
-    for id,seq in fasta_parser(fasta_file).items():
-        if id in list.keys():
-            id = re.sub(" .*", "", id )
-            print (id )
-
-    return
+if __name__ == "__main__":
+    main()
