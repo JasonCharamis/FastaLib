72b4abc98b673e6fa8431e69c55fd9d2d9f1618c
JasonCharamis <jason.charamis@gmail.com>
2023-12-08 15:35:51 +0200

    Small corrections

--- fastalib.py
+++ fastalib.py
@@ -31,11 +31,12 @@ def isfile(input_file, field=0): # Function to check if input is a file or a str
     return glists
 
 
+
 class FASTA:
 
     __slots__ = ['id','seq']
     
-    def __init__ (self, header, sequence):
+    def __init__ (self, header = "", sequence = ""):
         self.id = str(header)
         self.seq = str(sequence)
         
@@ -55,13 +56,13 @@ class FASTA:
 
             for line in lines:
                 line = line.strip('\n')  # Remove newline character
-               
+                
                 if line.startswith('>'):
                     seqid = re.sub(".*name=|\-0000\d|\.t\d","", line[1:])
                     sequence = ""
-                        
+                    
                 elif not re.search("--|#", line):  # Ignore lines containing "--" or "#"
-                    sequence += re.sub("\*$|\.$","",line)
+                    sequence += re.sub("\*$|\.$","",re.sub("\W","",line))
 
                 if seqid and sequence:
                     entries[seqid]=sequence
@@ -69,9 +70,9 @@ class FASTA:
         for seqids, sequences in entries.items():
             fasta_instance = FASTA(seqids, sequences)
             fasta_instances.append(fasta_instance)
-                   
+            
         return fasta_instances
-   
+    
         
     def fasta_sizer ( fasta_file): ## sizer of fasta sequences, takes dictionary as input ##
         lst = []
@@ -81,27 +82,27 @@ class FASTA:
         for fasta_instance in fasta_instances:
             fasta_size = '\t'.join([fasta_instance.id, str(len(fasta_instance.seq))])
             lst.append (fasta_size)
-           
+            
         sorted_list = natsorted(lst, key=lambda x: x[1], reverse=False) # sort based on sequence length
-       
+        
         return sorted_list
 
        
-     def fasta_extract(fasta_file, gene_list):
-        subset = []
-        fasta_instances = FASTA.fasta_parser(fasta_file)
+    def fasta_extract(fasta_file, gene_list):
+         subset = []
+         fasta_instances = FASTA.fasta_parser(fasta_file)
 
-        if isinstance(isfile(gene_list),list):       
+         if isinstance(isfile(gene_list),list):       
             subset = list(set([ fasta_instance for fasta_instance in fasta_instances for g in isfile(gene_list) if re.search(g, fasta_instance.id) ]))
 
-        else:
+         else:
             subset = list(set([ fasta_instance for fasta_instance in fasta_instances if re.search(isfile(gene_list), fasta_instance.id) ]))
 
-        if len(subset) > 0:            
-            return subset
+            if len(subset) > 0:            
+                return subset
         
-        else:
-            print ( "Gene list is empty or not provided." )
+            else:
+                print ( "Gene list is empty or not provided." )
 
             
     def fasta_remove ( fasta_file, gene_list ):
@@ -113,7 +114,7 @@ class FASTA:
 
         else:
             subset = list(set([ fasta_instance for fasta_instance in fasta_instances if not re.search(isfile(gene_list), fasta_instance.id) ]))
-                
+            
         if len(subset) > 0:            
             return subset
         
@@ -122,29 +123,29 @@ class FASTA:
 
     def translate(fasta_file):
         codon2aa = {
-           'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
-           'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
-           'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
-           'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
-           'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
-           'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
-           'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
-           'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
-           'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
-           'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
-           'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
-           'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
-           'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
-           'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
-           'TAC':'Y', 'TAT':'Y', 'TAA':'', 'TAG':'',
-           'TGC':'C', 'TGT':'C', 'TGA':'', 'TGG':'W'
-       }
+            'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
+            'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
+            'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
+            'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
+            'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
+            'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
+            'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
+            'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
+            'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
+            'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
+            'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
+            'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
+            'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
+            'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
+            'TAC':'Y', 'TAT':'Y', 'TAA':'', 'TAG':'',
+            'TGC':'C', 'TGT':'C', 'TGA':'', 'TGG':'W'
+        }
 
        
         fasta_instances = FASTA.fasta_parser(fasta_file)
 
         protein_fasta = []
-       
+        
         for fasta_instance in fasta_instances:
             fasta_instance.seq = fasta_instance.seq.upper() ## Convert CDS to uppercase
 
@@ -153,7 +154,7 @@ class FASTA:
 
             else:
                 aminoacids = []
-    
+                
                 for i in range(0,len(fasta_instance.seq),3):
                     codon = fasta_instance.seq[i:i+3]
                     
@@ -171,6 +172,44 @@ class FASTA:
 
         else:
             print ( "Provided fasta file is empty.")
+
+
+    def check_position (fasta_file, seq_name, position_number, length = 0):
+        '''
+        Returns the sequence from a fasta file, based on position number.
+        '''
+        
+        fasta_instances = FASTA.fasta_parser(fasta_file)
+
+        sequence = None
+        sequences = []
+
+        for fasta_instance in fasta_instances:
+            if re.search(seq_name, fasta_instance.id):
+                sequence = fasta_instance
+                break
+
+        if sequence:                      
+            if not position_number > len(sequence.seq) :
+                if length > 0:
+                    end_position = position_number + length
+                    sequences.append(sequence.seq[position_number:end_position])
+                    return ''.join(sequences)
+
+                elif length < 0:
+                    start_position = position_number - length
+                    sequences.append(sequence.seq[start_position:position_number])
+                    return ''.join(sequences)
+              
+                else:
+                    return sequence.seq[position_number + length] 
+
+            else:
+                print ( f"Requested position number not present in {fasta_instance.name}" )
+                return None
+        else:
+            print ( "Requested sequence does not exist in" + fasta_file )
+            return None
 
 
 
@@ -194,7 +233,13 @@ def parse_arguments():
     parser.add_argument('-s','--size', action="store_true", help='Option to print sizes of FASTA sequences.')
 
     ## option to convert CDS to PEP 
-    parser.add_argument('-tr','--cds2pep', action="store_true", help='Option to print sizes of FASTA sequences.')   
+    parser.add_argument('-tr','--cds2pep', action="store_true", help='Option to print sizes of FASTA sequences.')
+
+    ## get sequence based on position number
+    parser.add_argument('-pos','--position', action="store_true", help='Option to return requested position number of FASTA sequence.')
+    parser.add_argument('-seq_name','--sequence_name', type=str, help='Name of sequence to return requested position from a number of FASTA sequence.')
+    parser.add_argument('-num','--number', type=int, help='Position number to requested sequence from a FASTA sequence.')
+    parser.add_argument('-len','--length', type=int, help='Length to add to position number to requested sequence from a FASTA sequence. e.g. 280 + length')
         
     args = parser.parse_args()
 
@@ -236,15 +281,29 @@ def main():
             with open(f"{inp}.{args.gene_list}.removed.fasta", "w") as f:
                 for out in FASTA.fasta_remove(args.fasta, args.gene_list, args.field):
                     print ( out, file = f )
-               
+                    
         elif args.cds2pep:
             with open(f"{inp}.pep.fasta", "w") as f:
                 for out in FASTA.translate(args.fasta):
-                    print ( out, file = f )              
-    
+                    print ( out, file = f )
+
+        elif args.position:
+            if args.sequence_name:
+                if args.number:
+                    print ( "Sequence in requested site is" )
+                    print ( FASTA.check_position ( args.fasta, args.sequence_name, args.number, args.length if args.length else 0 ) )
+                else:
+                    print ("Please provide a position number to return.")
+            else:
+                print ( "Please provide the name of the sequence to search for returning the position number.")
+
+                
+        else:
+            print("Please select a potential FASTA operation.")
+
     else:
-        print("Please select a potential FASTA operation.")
-               
+        print ( "Please provide the input fasta file.")
+        
 
 if __name__ == "__main__":
     main()
